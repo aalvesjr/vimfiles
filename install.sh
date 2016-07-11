97566a7828d8428bc2950b56525e639a9ec676de
Armando <aalves.job@gmail.com>
2016-07-11 11:08:24 -0300

    Refactoring script install

--- install.sh
+++ install.sh
@@ -3,14 +3,17 @@
 VIMFILES_PATH=${HOME}/.vim
 VIMRC=${HOME}/.vimrc
 VIMFILES_URL='https://github.com/aalvesjr/vimfiles.git'
+VIMTMP=${HOME}/.vim-tmp
+VIM_OLD=${HOME}/.vim-old
+VIMRC_OLD=${HOME}/.vimrc-old
 
 echo "Cloning vimfiles from ${VIMFILES_URL}"
-git clone ${VIMFILES_URL} ${HOME}/.vim-tmp
+git clone ${VIMFILES_URL} ${VIMTMP}
 
 if [[ $? -eq 0 ]]; then
   if [[ -d ${VIMFILES_PATH} ]]; then
-    echo "Already exists one previous installation of vim, moving '${VIMFILES_PATH}' to '${HOME}/.vim-old'"
-    mv ${VIMFILES_PATH} ${HOME}/.vim-old
+    echo "Already exists one previous installation of vim, moving '${VIMFILES_PATH}' to '${VIM_OLD}'"
+    mv ${VIMFILES_PATH} ${VIM_OLD}
   fi
 
   mv ${HOME}/.vim-tmp $VIMFILES_PATH
@@ -19,15 +22,15 @@ if [[ $? -eq 0 ]]; then
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
   if [[ -s ${VIMRC} ]]; then
-    echo "Moving previous '${VIMRC}' to '${HOME}/.vimrc-old'"
-    mv ${VIMRC} ${HOME}/.vimrc-old
+    echo "Moving previous '${VIMRC}' to '${VIMRC_OLD}'"
+    mv ${VIMRC} ${VIMRC_OLD}
   fi
 
   echo "Creating link from '~/.vim/vimrc' to '~/.vimrc'"
-  ln -s ${VIMFILES_PATH}/vimrc ${HOME}/.vimrc
+  ln -s ${VIMFILES_PATH}/vimrc ${VIMRC}
 
   echo "Installing Plugins ..."
-  vim +PluginInstall +qall +GoInstallBinaries > /dev/null 2>&1
+  vim +PluginInstall +GoInstallBinaries +qall > /dev/null 2>&1
 
   echo "[ OK ] VIM configured!"
 
